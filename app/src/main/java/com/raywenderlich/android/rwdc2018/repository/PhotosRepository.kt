74fe97d39d5b6ad54b61a70a5615fda839a88e34
animesh <royanimesh2211@gmail.com>
2020-04-21 01:43:04 +0530

    Deleted unncecessary import

--- app/src/main/java/com/raywenderlich/android/rwdc2018/repository/PhotosRepository.kt
+++ app/src/main/java/com/raywenderlich/android/rwdc2018/repository/PhotosRepository.kt
@@ -33,10 +33,6 @@ package com.raywenderlich.android.rwdc2018.repository
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.util.Log
 import com.raywenderlich.android.rwdc2018.app.PhotosUtils
 
